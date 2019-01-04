d5083322199d31414df48878bf092cb6753847b2
Yaming Gong <ymgongcn@gmail.com>
2019-01-04 16:50:53 +0800

    Add other implementation

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -5,7 +5,7 @@ func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
 
-// SquareOfSum the square of the sum of the first n natural numbers
+/* SquareOfSum the square of the sum of the first n natural numbers
 func SquareOfSum(n int) int {
 	sum := 0
 	for i := 0; i <= n; i++ {
@@ -13,12 +13,29 @@ func SquareOfSum(n int) int {
 	}
 	return sum * sum
 }
+*/
 
-// SumOfSquares the sum of the squares of the first n natural numbers
+// SquareOfSum without loop
+func SquareOfSum(n int) int {
+	sum := (n * (n + 1)) / 2
+	return sum * sum
+}
+
+/* SumOfSquares the sum of the squares of the first n natural numbers
 func SumOfSquares(n int) int {
 	sum := 0
 	for i := 0; i <= n; i++ {
 		sum += i * i
 	}
 	return sum
+}
+*/
+
+// SumOfSquares using recursive
+func SumOfSquares(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	return n*n + SumOfSquares(n-1)
 }
