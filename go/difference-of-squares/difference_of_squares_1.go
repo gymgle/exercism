5b2d3d11c20e10207d69110625c0ea30bc16cd69
Yaming Gong <ymgongcn@gmail.com>
2019-01-05 17:06:55 +0800

    Add other implementation

--- go/difference-of-squares/difference_of_squares_1.go
+++ go/difference-of-squares/difference_of_squares_1.go
@@ -1,17 +1,23 @@
 package diffsquares
 
+/*
 // Difference the difference between the square of the sum
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
 
-// SquareOfSum with a standard algorithm
+// SquareOfSum without loop
 func SquareOfSum(n int) int {
 	sum := (n * (n + 1)) / 2
 	return sum * sum
 }
 
-// SumOfSquares with a standard algorithm
+// SumOfSquares using recursive
 func SumOfSquares(n int) int {
-	return n * (n + 1) * (2*n + 1) / 6
+	if n == 0 {
+		return 0
+	}
+
+	return n*n + SumOfSquares(n-1)
 }
+*/
